7914311fee21912ddf16f1444b2dabd18298e188
Rob Garrett <rob@robgarrett.com>
2015-12-29 15:17:14 -0500

    Updated settings

--- Install/Settings/settings-exampleserver.ps1
+++ Install/Settings/settings-exampleserver.ps1
@@ -1,21 +1,40 @@
 ######################################
 ## Example Server Settings
 
+# Prefix for all databases.
 $global:dbPrefix = "MyFarm";
-$global:passphrase = "MyPassPhrase";
+# SQL Server
 $global:dbServer = "MySQLServer";
-$global:serverRole = "Application";
 
+# Server Role (SharePoint 2016+).
+$global:serverRole = "Application";
+# Farm passphrase.
+$global:passphrase = "MyPassPhrase";
+# Port for Central Admin.
+$global:CAportNumber = 2016;
+
+# Accounts.
+$:global:spFarmAcctName = "DOMAIN\spfarm";
 $global:spAdminAcctName = "DOMAIN\spadmin";
 $global:spServiceAcctName = "DOMAIN\spservice";
 $global:spServiceAcctPwd = "mypassword";
 $global:spAppPoolAcctName = "DOMAIN\spapppool";
 $global:spAppPoolAcctPwd = "mypassword";
-$global:CAportNumber = 2016;
+$global:spSearchCrawlAcctName = "DOMAIN\spsearch";
+$global:spSearchCrawlAcctPwd = "mypassword";
+
+# Logging Settings.
 $global:logDaysToKeepLogs = 2;
 $global:logSpaceUsage = 5;
+
+# SMTP Server for Outgoing Email.
 $global:smtpServer = "localhost";
 $global:fromEmailAddress = "no-reply@myserver.com";
 
-
+# Location for search indexes.
+$global:indexLocation = "c:\SPIndex";
+# Array of servers with query components.
+$global:queryServers = ($env:COMPUTERNAME);
+# Array of servrs with crawl components.
+$global:crawlServers = ($env:COMPUTERNAME);
 
