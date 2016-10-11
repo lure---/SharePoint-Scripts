7e9fa6a1788f4aa93fecbd932024153ff675e5a4
Rob Garrett <rob@robgarrett.com>
2016-10-11 13:16:12 -0400

    Added Azure LB scripts and SP roles

--- Azure/Start-VMs.ps1
+++ Azure/Start-VMs.ps1
@@ -34,11 +34,21 @@ try {
     # Start the VMs
     $vmName = "$($ResourceGroupName.ToLower())-sql";
     StartAndWait-ForVM -vmName $vmName -resourceGroupName $ResourceGroupName;
-    $vmName = "$($ResourceGroupName.ToLower())-spapp";
-    StartAndWait-ForVM -vmName $vmName -resourceGroupName $ResourceGroupName;
-    $vmName = "$($ResourceGroupName.ToLower())-spwfe";
-    StartAndWait-ForVM -vmName $vmName -resourceGroupName $ResourceGroupName;
-	
+    # Start APP and WFE in parallel 
+    $spAppVmName = "$($ResourceGroupName.ToLower())-spapp";
+    $spWfeVmName = "$($ResourceGroupName.ToLower())-spwfe";
+    $spAppVmName, $spWfeVmName | Invoke-Parallel -Parameter $ResourceGroupName -ScriptBlock {
+        $vmName = $_;
+        $resourceGroupName = $parameter;
+        $adPowerState = (Get-AzureRMVM -ResourceGroupName $resourceGroupName -Name $vmName -Status | `
+            Select-Object -ExpandProperty Statuses | Where-Object {$_.Code -like '*PowerState*'} | `
+            Select-Object @{l='PowerState';e={$_.Code.Split('/')[1]}}).PowerState;
+        Write-Verbose "Powerstate of $vmName is $adPowerState";
+        If ($adPowerState -ine "Running" ) {
+            Write-Host -ForegroundColor Yellow "Starting VM $vmName";
+            Start-AzureRmVM -Name $vmName -ResourceGroupName $resourceGroupName -Verbose; 
+        }
+    }    
 } catch {
 	Write-Error $_.Exception;
 }
